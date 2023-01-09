632c6d5a7cde14c7cf2a17a66930a106cd1599b1
adrienm7 <moyaux.adrien@gmail.com>
2023-01-10 00:54:34 +0100

    Correction du problème de hauteur du footer

--- src/composants/Footer.svelte
+++ src/composants/Footer.svelte
@@ -8,4 +8,19 @@
 </footer>
 
 <style>
+	footer {
+		display: block;
+		margin: 0;
+		margin-top: 10vh;
+		padding: 2px;
+		background-color: rgba(0, 0, 0, 0.8);
+		color: white;
+		box-shadow: 0 0 3px 2px var(--couleur-ombre);
+	}
+
+	footer p {
+		font-size: 0.9rem;
+		text-align: center;
+	}
+
 </style>
