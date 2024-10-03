bc1a9ac3c5a3444b4638bef6bc75f1fba449eb0f
adrienm7 <49562406+adrienm7@users.noreply.github.com>
2024-10-03 09:40:22 +0000

    Correction v2

--- .cpanel-ci.js
+++ .cpanel-ci.js
@@ -8,7 +8,7 @@ async function deployToCPanel() {
     server: process.env.FTP_SERVER,
     username: process.env.FTP_USERNAME,
     password: process.env.FTP_PASSWORD,
-   " local-dir": "./build/",
+   "local-dir": "./build/",
     timeout: 600000,
     "dry-run": false,
     protocol: "ftps",
