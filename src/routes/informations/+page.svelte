a2850cec914b6e32850a5a55ca2d934c90ce3836
adrienm7 <moyaux.adrien@gmail.com>
2024-10-06 13:01:27 +0200

    Nettoyage de nodemailer qui n’est plus utilisé

--- src/routes/informations/+page.svelte
+++ src/routes/informations/+page.svelte
@@ -1,31 +1,4 @@
 <script>
-	// import nodemailer from 'nodemailer';
-	// import { onMount } from 'svelte';
-
-	// onMount(() => {
-	// 	const transporter = nodemailer.createTransport({
-	// 		host: 'mail.beseven.fr', // Remplacez par le nom de votre serveur SMTP
-	// 		port: 465, // Remplacez par le port de votre serveur SMTP
-	// 		secure: false,
-	// 		auth: {
-	// 			user: 'hypertexte@beseven.fr', // Remplacez par votre nom d'utilisateur SMTP
-	// 			pass: '~=qqrVHC_wS8' // Remplacez par votre mot de passe SMTP
-	// 		}
-	// 	});
-
-	// 	let name = 'uie';
-	// 	let email = 'aie@meu.fr';
-	// 	let message = 'uei';
-
-	// 	const mailOptions = {
-	// 		from: email,
-	// 		to: 'moyaux.adrien@gmail.com',
-	// 		subject: `Message from ${name}`,
-	// 		text: message
-	// 	};
-
-	// 	transporter.sendMail(mailOptions);
-	// });
 	import Nom from '$lib/composants/Nom.svelte';
 	import NomPlus from '$lib/composants/NomPlus.svelte';
 	import SFB from '$lib/composants/SFB.svelte';
