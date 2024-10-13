948978fbc67cac8f1951307aa410538b6a232d52
adrienm7 <moyaux.adrien@gmail.com>
2024-10-13 11:43:35 +0200

    Ajout d’un paragraphe sur la licence

--- src/routes/informations/licence.svelte
+++ src/routes/informations/licence.svelte
@@ -5,4 +5,35 @@
 </script>
 
 <h2>Licence</h2>
-<p>À définir</p>
+
+<p>
+	La disposition de clavier <Nom /> ainsi que son site de présentation
+	<a href="https://hypertexte.beseven.fr">hypertexte.beseven.fr</a>
+	sont distribués sous la licence <strong class="important">MIT</strong>. Il s'agit d'une licence
+	permissive, simple et courte, qui n'impose que quelques conditions, notamment la préservation des
+	mentions de droits d'auteur et de licence.
+</p>
+<p>
+	Les travaux sous cette licence, y compris les modifications et œuvres dérivées, peuvent être
+	redistribués sous d'autres termes, et ce, même sans fournir le code source. La seule condition est
+	de conserver les mentions de licence et de droits d'auteur.
+</p>
+
+<h3>Permissions</h3>
+<ul>
+	<li>Utilisation commerciale ;</li>
+	<li>Modification ;</li>
+	<li>Distribution ;</li>
+	<li>Usage privé.</li>
+</ul>
+
+<h3>Limitations</h3>
+<ul>
+	<li>Absence de garantie ;</li>
+	<li>Aucune responsabilité.</li>
+</ul>
+
+<h3>Conditions</h3>
+<ul>
+	<li>Conservation des mentions de licence et des droits d'auteur.</li>
+</ul>
