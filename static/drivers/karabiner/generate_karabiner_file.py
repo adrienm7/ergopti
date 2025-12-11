402285299664c7296cf2fe7344e69e9b1778b0d4
adrienm7 <moyaux.adrien@gmail.com>
2025-12-11 15:01:51 +0100

    Clean plus mappings extension

--- static/drivers/karabiner/generate_karabiner_file.py
+++ static/drivers/karabiner/generate_karabiner_file.py
@@ -30,7 +30,7 @@ from utilities.rolls_mappings import (
 REPEAT_KEY = False
 plus_mappings = add_case_sensitive_mappings(PLUS_MAPPINGS_CONFIG)
 # Save plus_mappings to a file named 'plus_mappings' in the current directory
-plus_mappings_path = Path(__file__).parent / "plus_mappings"
+plus_mappings_path = Path(__file__).parent / "plus_mappings.json"
 with open(plus_mappings_path, "w", encoding="utf-8") as f:
     json.dump(plus_mappings, f, ensure_ascii=False, indent=2)
 
