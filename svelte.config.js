e79fd8ab89ae5ba38995833eebc6bb3b21322742
adrienm7 <moyaux.adrien@gmail.com>
2026-03-16 15:22:53 +0100

    test fix 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
+// Allow overriding the base path at build time via BASE_PATH environment variable.
+const base = process.env.BASE_PATH || '';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
 			precompress: true
-		})
+		}),
+		paths: {
+			base
+		}
 	}
 };
 
