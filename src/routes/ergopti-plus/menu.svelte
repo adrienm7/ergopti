64c2ed918fcb6856974e7440ef4c62731421e029
adrienm7 <moyaux.adrien@gmail.com>
2026-03-16 10:58:15 +0100

    Add new Hammerspoon docs + clean drivers folder

--- src/routes/ergopti-plus/menu.svelte
+++ src/routes/ergopti-plus/menu.svelte
@@ -8,18 +8,24 @@
 	<h2 class="first-h2">Menu de gestion des fonctionnalités</h2>
 
 	<p>
-		Sur Windows, la version AutoHotkey est extrêmement complète. Elle possède même désormais un menu
-		de gestion de chacune des fonctionnalités implémentées :
+		Sur Windows et macOS, les versions <ErgoptiPlus></ErgoptiPlus> possèdent même désormais un menu de
+		gestion de chacune des fonctionnalités implémentées :
 	</p>
 	<enhanced:img
 		src="$lib/images/menu_ergopti_plus.jpg"
 		alt="Screenshot du menu du script Ergopti+"
 	/>
+	<tiny-space></tiny-space>
+	<enhanced:img
+		src="$lib/images/ergopti_plus_macos.png"
+		alt="Screenshot du menu du script Ergopti+ sur macOS"
+	/>
 
 	<p>
 		Un clic droit sur l’icône du script dans la barre des tâches permet d’ouvrir le menu du script.
-		Chaque feature y est visible et peut être activée ou désactivée, avec toutes les autres features
-		activées qui restent fonctionnelles. Ce menu permet de voir rapidement toutes les possibilités
-		d’<ErgoptiPlus></ErgoptiPlus> et de désactiver les fonctionnalités dont vous ne voulez pas.
+		Chaque fonctionnalité y est visible et peut être activée ou désactivée, avec toutes les autres
+		features activées qui restent fonctionnelles. Ce menu permet de voir rapidement toutes les
+		possibilités d’<ErgoptiPlus></ErgoptiPlus> et de désactiver les fonctionnalités dont vous ne voulez
+		pas.
 	</p>
 </section>
